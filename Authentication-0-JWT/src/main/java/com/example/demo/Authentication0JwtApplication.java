1121343a7587f193b883c7f5d0cf0e551f29d1d9
TranDacKy <trandackydbz2@gmail.com>
2020-11-02 18:47:33 +0700

    first commit

---
+++ Authentication-0-JWT/src/main/java/com/example/demo/Authentication0JwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Authentication0JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Authentication0JwtApplication.class, args);
+	}
+
+}
